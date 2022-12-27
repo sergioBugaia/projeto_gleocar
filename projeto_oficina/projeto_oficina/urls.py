3b8787fdc37f7305aeb93059e7fcdfb8d21aeb24
sergioBugaia <santoslsergio@yahoo.com>
2022-12-27 16:41:13 -0300

    Construindo o Admin

--- projeto_oficina/projeto_oficina/urls.py
+++ projeto_oficina/projeto_oficina/urls.py
@@ -19,7 +19,7 @@ from cliente import views
 from cliente import views as views_registrado
 
 urlpatterns = [
-    #path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('',views.pagina_inicial),
     path('novo/', views.novo_cliente, name='novo_cliente'),
     path('registrado/', views_registrado.cliente_registrado, name='cliente_registrado')
