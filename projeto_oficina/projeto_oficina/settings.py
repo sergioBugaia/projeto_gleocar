b8cf2957543e6a3e9b576c3bae9a54d3d9944c24
sergioBugaia <santoslsergio@yahoo.com>
2023-02-10 15:15:00 -0300

    add Procfile e inclusao  linha 1 e 2 do gitignore

--- projeto_oficina/projeto_oficina/settings.py
+++ projeto_oficina/projeto_oficina/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-s)v6deeqx%=et7j*1rr(ffp(j+essa0fgb!2p-@c59awb5udco
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -123,10 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles") 
-STATICFILES_DIRS = [
-  os.path.join(BASE_DIR, "static")  
-] 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")] 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Default primary key field type
